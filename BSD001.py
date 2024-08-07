3dd3a8b53aa84a523e6f48d4eff5917919f1aed6
Pavel-008 <viguropasha@gmail.com>
2024-08-07 21:01:28 +0500

    Add files via upload

---
+++ BSD001.py
@@ -0,0 +1,27 @@
+print(5) # Типы данных
+print(type(5))
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(5//5) # // целочисленное деление, / - деление
+print(5%5)
+print(5**5)
+print(2.0)
+print(type(2.0))
+print(2.0+2.0)
+print(2.0+2) # Строки
+print('Hello, world!') # - без ковычек выводит ошибку print(Hello, world!)
+print(type('Hello, world!')) # string - строка
+print('Hello,' + 'world!')
+print('1'+'1') # concatenate, разные типы данных не складываются print('1'+1). # Логический тип данных.
+print(True, False) # они указывают на истину или ложность того, или иного выражения
+print(type(True), type(False)) # boolen
+print(5>10)
+print(5>10, 10>5)
+print(5==5) # >,<,>=,>=,== - равенство обьектов, != - не равенство обьектов.
+print(5 != 5)
+print(5!= 5 and 5<10) # команды and - и, or - или.
+print(5!= 5 or 5<10) # Изменения типа данных.
+print(int('5'))
+print(type(int('5'))) # логично что текст  перевести в числовой тип данных мы не можем.
---
+++ BSD002.py
@@ -0,0 +1,8 @@
+print(12+89-1)
+print(9**0.5*5) # 1st program
+print(9.99>9.98 and 1000!=1000.1) # 2st program
+print((2*2+2) == (2*(2+2))) # 3st program (print((int('123.456'))*10) - ошибка)
+print('123' + '.456') # 4st program (print((int('123.456'))) - ошибка)
+print(123.456*10)
+print(1234.56-1230.56)
+print(int('4'))
